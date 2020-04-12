4c5e2d60b0b1241b9470434be3a1b0f979071214
Hector Flores <Hector_Flores@eogresources.com>
2020-04-11 21:50:00 -0700

    LET and CONST, Multiline, Spread Operator and Destructuring

--- src/es6/index.js
+++ src/es6/index.js
@@ -27,3 +27,55 @@ console.log(epicPhrase);
 // Template Literal es6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+// Multiline before es6
+
+let lorem = 'phrase 1 \n' + 'phrase 2';
+
+// Multiline es6
+let lorem2 = `phrase 1
+phrase2`;
+
+console.log(lorem);
+console.log(lorem2);
+
+// Destructuring before es6
+let person = {
+  name: 'hector',
+  age: 28,
+  country: 'VEN',
+};
+
+console.log(person.name, person.age, person.country);
+
+// Destructuring es6
+
+let { name, age, country } = person;
+console.log(name, age, country);
+
+// spread operator
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yessica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+// LET
+
+{
+  let globalLet = 'Global LET';
+  console.log(globalLet);
+}
+
+// VAR
+{
+  var globalVar = 'Global VAR';
+}
+console.log(globalVar);
+
+// CONST
+
+const a = 'b';
+a = 'a';
+console.log(a);
