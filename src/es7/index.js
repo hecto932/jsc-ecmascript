7fbcf8917b7ca9f5cb79e42b847679356eac5466
Hector Flores <Hector_Flores@eogresources.com>
2020-04-12 12:08:19 -0700

    Includes on arrays and POW with **

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+// INCLUDES
+
+let numbers = [1, 2, 3, 7, 8];
+if (numbers.includes(7)) {
+  console.log('It exist number 7');
+} else {
+  console.log('It doesnt exist number 7');
+}
+
+// POW **
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
