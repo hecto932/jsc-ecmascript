ea749a2faf11888bd42460d32a127f63d79b44a4
Hector Flores <Hector_Flores@eogresources.com>
2020-04-12 13:42:03 -0700

    ES8: Async/Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -29,3 +29,29 @@ const data2 = {
 };
 
 console.log(data2);
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve('Hello world!'), 3000)
+      : reject(new Error('Test error...'));
+  });
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+anotherFunction();
